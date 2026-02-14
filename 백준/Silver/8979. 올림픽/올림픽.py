176b9d76db16e0ac189beb8144a68b5d283a83ec
sff14123 <sff14123@gmail.com>
2026-02-14 20:53:46 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 110964 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,23 +1,24 @@
 n, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-rank = []
-for i in range(n-1):
-    if arr[i][1] < arr[i+1][1]: # 금메달 비교
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-    elif arr[i][1] == arr[i+1][1]:
-        if arr[i][2] < arr[i+1][2]: # 은메달 비교
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-        elif arr[i][2] == arr[i+1][2]:
-            if arr[i][3] < arr[i+1][3]: # 동메달 비교
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-# 동일 점수 공동 등수로 만들기
+
 for i in range(n):
-    cnt = 0
+    for j in range(n - 1 - i):
+        if arr[j][1] < arr[j+1][1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+        elif arr[j][1] == arr[j+1][1]:
+            if arr[j][2] < arr[j+1][2]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            elif arr[j][2] == arr[j+1][2]:
+                if arr[j][3] < arr[j+1][3]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+
+target_idx = 0
+for i in range(n):
     if arr[i][0] == k:
-        cnt += 1
-    if cnt == 1:
-        print(i+1)
+        target_idx = i
         break
-    else:
-        print(i+2-cnt)
+
+for i in range(n):
+    if arr[i][1:4] == arr[target_idx][1:4]:
+        print(i + 1)
         break
