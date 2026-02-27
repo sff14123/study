fea7c6f5b3d03371dd9b500e0a265a162ea5f614
sff14123 <sff14123@gmail.com>
2026-02-27 15:00:34 +0900

    [Bronze I] Title: 세로읽기, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,6 @@
+import sys
+arr = [list(sys.stdin.readline().rstrip()) for _ in range(5)]
+for j in range(15):
+    for i in range(5):
+        if j < len(arr[i]):
+            print(arr[i][j], end='')
